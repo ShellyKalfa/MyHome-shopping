c729008535db62476cabb69006b8cb28543d65e6
ShellyKalfa <drsk261100@gmail.com>
2025-05-18 23:10:57 +0300

    hack rami lavi

--- shoppingList/frontend/src/components/ShoppingListFile.js
+++ shoppingList/frontend/src/components/ShoppingListFile.js
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import ShoppingList from "./ShoppingList";
-
+import axios from 'axios';
 
 
 export default function ShoppingListFile() {
+    const [item,setItem]=useState("")
+     const  handleSignUp = (event) => {
+        event.preventDefault();
+     axios.post('http://localhost:5000/search', {"aggs": 1,"q": "גבינה",  "store": 331})
+          .then(res => { 
+            
+            console.log(res.data);
+            setItem(res.data.data[0].name);
+
+          })
+          .catch(err => {console.log(err.response.data)})
+
+     
+    }
     return ( 
 <div>
     <ShoppingList/>
+    <button onClick={handleSignUp}>Sign Up</button>
+    <div> {item}</div>
 </div>
 
      )
