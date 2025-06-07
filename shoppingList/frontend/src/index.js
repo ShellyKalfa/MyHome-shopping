5b6127e4c1fdb0edf0fbce1b4f7a6f6bb7406006
omer levy <102253977+omerlevyk@users.noreply.github.com>
2025-06-08 00:28:50 +0300

    omer's push (#6)

--- shoppingList/frontend/src/index.js
+++ shoppingList/frontend/src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import{BrowserRouter} from "react-router-dom"
+import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <GoogleOAuthProvider clientId="460664212191-bnf64k7d5lok2kckohjdtafsr1egf7mf.apps.googleusercontent.com">
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </GoogleOAuthProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
