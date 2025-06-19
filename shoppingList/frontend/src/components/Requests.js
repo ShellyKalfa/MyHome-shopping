984d28eed03b836471fe3c53df3774d3a37ab2ae
ShellyKalfa <drsk261100@gmail.com>
2025-06-19 04:19:47 +0300

    item.approved === 0

--- shoppingList/frontend/src/components/Requests.js
+++ shoppingList/frontend/src/components/Requests.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react';
 export default  function Requests({items} ) {
 const [approvedNanItems, setApprovedNanItems] = useState(items.filter(item => item.approved === 0));
  useEffect(() => {
-    setApprovedNanItems(items.filter(item => item.approved === 1));
+    setApprovedNanItems(items.filter(item => item.approved === 0));
   }, [items]);
 
     return (<div> 
